8783c7bab55127320d12de3c4b8f3e330ae5a8fc
Mathieui GOULIN <mathieu.goulin@gadz.org>
2020-02-29 22:04:13 +0100

    [FIX] plg_notify_exec produce defunc

--- src/plugins/plg_notify_exec.c
+++ src/plugins/plg_notify_exec.c
@@ -105,20 +105,25 @@ void handle_event(char *p_name, struct directory *dir, const struct inotify_even
 
 	char *cmd = malloc(sizeof(char) * (strlen(get_config(config_cmd)) + strlen(dir->name) + strlen(event->name) + strlen(value) + 1));
 	sprintf(cmd, get_config(config_cmd), dir->name, event->name, value);
-	log_msg("DEBUG", "EXECUTE CMD: %s", cmd);
+	log_msg("DEBUG", "Execute cmd: %s", cmd);
         char *args[]={"/bin/bash","-c",cmd,NULL};
+	int status;
 	pid_t pid = fork();
 	if (pid == -1){
 		log_msg("ERROR", "can't fork, error occured");
 		exit(EXIT_FAILURE);
 	} else if (pid == 0){
 		// child process
-	        if(execvp(args[0],args) < 0)
+		int execpid=execvp(args[0],args);
+	        if(execpid < 0)
 		{
 			log_msg("ERROR", "plg_notify_exec.c: Le retour de exevp est KO");
 		}
 		exit(EXIT_SUCCESS);
 	}
+	while (wait(&status) != pid)       /* wait for completion  */
+               ;
+	log_msg("DEBUG", "Cmd return status = %i", status);
 	free(config_cmd);
 	free(cmd);
 	//	log_msg("INFO", "[%s] %s : %s/%s %s", type, dir->key, dir->name, event->name, isdir);
